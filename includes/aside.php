fd232f6c3ee39ac3d7c3a1891ca618e619cb215a
ISAUL <isaul92@outlook.es>
2019-10-12 21:56:33 -0500

    segundo committ

--- includes/aside.php
+++ includes/aside.php
@@ -1,53 +1,50 @@
-<?php ?>
-
-      <!--   BARRA LATERAL  <>    -->
-      
-            <?php 
-                    
-                    if (isset($_SESSION["errores"])) {
-                        
-                        
-                        var_dump($_SESSION["errores"]);
-                        
-                        
-                    }
-                    ?>
-      <div id="contenedor">
-            
-            
-            
-            <aside id="aside">
 
 
-                <div id="login" class="block-aside">
-                    <h3>Identificate</h3>
-                    <form action="login.php" method="POST">
-                        <label>Email</label>
-                        <input type="email" name="email" placeholder="Ingrese email">
-                        <label>Contraseña</label>
-                        <input type="password" name="contraseña" required="required" placeholder="Ingrese contraseña">
+<!--   BARRA LATERAL  <>    -->
+<div id="contenedor">
+    <aside id="aside">
 
-                        <input type="submit"  required="required "value="Acceder">
-                    </form>
-                </div>
 
-                <div id="login" class="block-aside">
-                    <h3>Registrate</h3>
-                    
-                    
-              
-                    <form action="registrar.php" method="POST">
+        <div id="login" class="block-aside">
+            <h3>Identificate</h3>
+            <form action="login.php" method="POST">
+                <label>Email</label>
+                <input type="email" name="email" placeholder="Ingrese email">
+                <label>Contraseña</label>
+                <input type="password" name="contraseña" required="required" placeholder="Ingrese contraseña">
 
-                        <label>Nombre</label>
-                        <input type="text" name="nombre" placeholder="Ingrese su nombre">
-                        <label>Apellidos</label>
-                        <input type="text" name="apellidos" placeholder="Ingrese sus apllidos">
-                        <label>Email</label>
-                        <input type="email" name="email" placeholder="Ingrese email">
-                        <label>Contraseña</label>
-                        <input type="password" name="pass" required="required" placeholder="Ingrese contraseña">
+                <input type="submit"  required="required "value="Acceder">
+            </form>
+        </div>
 
-                        <input type="submit"  required="required" name="submit" value="Registrar">
-                    </form>
-                </div>
-            </aside>
+        <div id="login" class="block-aside">
+            <h3>Registrate</h3>
+
+
+            <?php
+            include 'includes/helpers.php';
+            if (isset($_SESSION["errores"])) {
+
+
+                //       var_dump($_SESSION["errores"]);
+            }
+            ?>
+            <form action="registrar.php" method="POST">
+
+                <label>Nombre</label>
+                <input type="text" name="nombre" placeholder="Ingrese su nombre">                          
+                <?php isset($_SESSION["errores"]) ? mostrarError($_SESSION["errores"], "nombre") : ""; ?>
+                <label>Apellidos</label>
+                <input type="text" name="apellidos" placeholder="Ingrese sus apllidos">
+                <?php isset($_SESSION["errores"]) ? mostrarError($_SESSION["errores"], "apellidos") : ""; ?>
+                <label>Email</label>
+                <input type="email" name="email" placeholder="Ingrese email">
+              <?php isset($_SESSION["errores"]) ? mostrarError($_SESSION["errores"], "email") : ""; ?>
+                <label>Contraseña</label>
+                <input type="password" name="pass"  placeholder="Ingrese contraseña">
+              <?php isset($_SESSION["errores"]) ? mostrarError($_SESSION["errores"], "pass") : ""; ?>
+                <input type="submit"  required="required" name="submit" value="Registrar">
+            </form>
+            <?php borrarErrores(); ?>
+        </div>
+    </aside>
--- includes/helpers.php
+++ includes/helpers.php
@@ -1,8 +1,18 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function mostrarError($errores,$campo){
+    $alerta="";
+    if(isset($errores[$campo]) && !empty($campo)){
+        $alerta ="<div class='alerta'>".$errores[$campo]."</div>";
+        
+        
+    }
+    return $alerta;
+}
 
+
+function borrarErrores(){
+    $_SESSION['errores']=null;
+    $borrado= session_unset($_SESSION['errores']);
+    return $borrado;
+}
